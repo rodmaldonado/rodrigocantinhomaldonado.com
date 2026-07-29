7e8f01fcf579191fe4c2fec701ef34744b8de6f4
rodmaldonado <rodmaldonado8@gmail.com>
2026-07-28 22:56:13 -0300

    Ampliar SEO com quatro artigos estratégicos

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 set -eu
 rm -rf public site.tar.gz
 mkdir -p public
-cat site.part.00 site.part.01 site.part.02 site.part.03 > site.tar.gz
+cat site.part.00 site.part.01 site.part.02 site.part.03 site.part.04 > site.tar.gz
 tar -xzf site.tar.gz -C public
